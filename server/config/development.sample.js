397661b846dd34836f6dd9ec1d8b3805ff458d65
lmaran <lucian.maran@outlook.com>
2017-12-29 14:33:45 +0000

    minor update [skip ci]

---
+++ server/config/development.sample.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const config = {
+    port: process.env.PORT || 1408,
+    mongo: {
+        uri: "mongodb://localhost/cantinas-dev"
+    },
+    authRootUrl: process.env.AUTH_ROOT_URL || "http://cantinas.ro"
+};
+exports.default = config;
